9bb004e405084a72f020a49e86559d4ffdda3066
Ineza-Agape <i.agape@alustudent.com>
2024-03-23 13:59:43 -0700

    kigali

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
--- lists all privileges of the MySQL users
--- user_0d_1 and user_0d_2
+-- List all privileges of the MySQL users
+-- User_0d_1 and user_0d_2
 
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
