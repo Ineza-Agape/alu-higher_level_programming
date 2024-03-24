392bcba632cb0d5f98e1fa69d8a11c0c0c5da5f0
Ineza-Agape <i.agape@alustudent.com>
2024-03-24 17:30:49 +0000

    net 11

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that fetches https://alu-intranet.hbtn.io/status
-- using the urllib package.
-"""
-import urllib.request
+"""Fetches https://alu-intranet.hbtn.io/status"""
+if __name__ == "__main__":
+    from urllib import request
 
-
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+    with request.urlopen("https://alu-intranet.hbtn.io/status") as f:
+        status = f.read()
+        print("Body response:\n"
+              "\t- type: {}\n"
+              "\t- content: {}\n"
+              "\t- utf8 content: {}".format(type(status), status,
+                                            status.decode("utf-8")))
